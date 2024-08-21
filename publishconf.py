a18073df4b83b39d5f01a7ff4388843788b6038d
Thomas Stucky <thomas@astrostucky.com>
2024-08-21 17:00:01 -0600

    site made https

--- publishconf.py
+++ publishconf.py
@@ -8,9 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-## TODO: enable https
-# SITEURL = "https://astrostucky.com"
-SITEURL = "http://astrostucky.com"
+SITEURL = "https://astrostucky.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
